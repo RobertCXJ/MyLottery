aa3dfa73c37504a533cde47c4a2944e684c7107e
RobertChen <2797037795@qq.com>
2024-04-09 20:31:10 +0800

    feat:抽奖策略领域开发

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/model/req/DrawReq.java
@@ -0,0 +1,37 @@
+package cn.itedus.lottery.domain.strategy.model.req;
+
+import cn.itedus.lottery.infrastructure.po.Strategy;
+
+public class DrawReq {
+
+    // 用户ID
+    private String uId;
+
+    // 策略ID
+    private Long strategyId;
+
+    public DrawReq() {
+    }
+
+    public DrawReq(String uId, Long strategyId) {
+        this.uId = uId;
+        this.strategyId = strategyId;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public Long getStrategyId() {
+        return strategyId;
+    }
+
+    public void setStrategyId(Long strategyId) {
+        this.strategyId = strategyId;
+    }
+
+}
